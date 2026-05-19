42756ee399407ad1187c95fb42ef89fb4429294a
unknown <vikaratmanova@gmail.com>
2026-05-19 10:49:46 +0300

    Updated analysis.py with def for finding uncovered districts and def for calculating buffer area in district

--- src/buffer_coverage/core/analysis.py
+++ src/buffer_coverage/core/analysis.py
@@ -42,3 +42,69 @@ def coverage_ratio(
     result_gdf["coverage_ratio"] = coverage_ratios
 
     return result_gdf
+
+
+def uncovered_areas(
+    districts_gdf: gpd.GeoDataFrame, objects_gdf: gpd.GeoDataFrame, radius: float
+) -> gpd.GeoDataFrame:
+    """
+    Returns uncovered sections of districts.
+    Args:
+        districts_gdf (gpd.GeoDataFrame): GeoDataFrame with polygons of districts.
+            It must contain geometry in any format (it will be converted to EPSG:3857).
+        objects_gdf (gpd.GeoDataFrame): GeoDataFrame with point or polygon
+            objects around which buffers are created. Must have the same coordinate system as the districts_gdf.
+        radius (float): The radius of the buffer zone in meters.
+    Return:
+        result_gdf (gpd.GeoDataFrame): A copy of the districts_gdf with the added 'coverage_ratio' column. Number (float) from 0 to 1.
+    """
+    check_not_empty(districts_gdf, "districts_gdf")
+    check_not_empty(objects_gdf, "objects_gdf")
+    check_same_crs(districts_gdf, objects_gdf)
+
+    districts = districts_gdf.to_crs(epsg=3857)
+    objects = objects_gdf.to_crs(epsg=3857)
+
+    buffers = create_buffers(objects, radius)
+    union_buffer = buffers.geometry.union_all()
+
+    uncovered_gdf = districts.copy()
+    uncovered_gdf.geometry = uncovered_gdf.geometry.difference(union_buffer)
+
+    return uncovered_gdf[~uncovered_gdf.geometry.is_empty]
+
+
+def coverage_area(
+    districts_gdf: gpd.GeoDataFrame, objects_gdf: gpd.GeoDataFrame, radius: float
+) -> gpd.GeoDataFrame:
+    """
+    Calculates the buffer area coverage for each district.
+
+    Args:
+        districts_gdf (gpd.GeoDataFrame): GeoDataFrame with polygons of districts.
+            It must contain geometry in any format (it will be converted to EPSG:3857).
+        objects_gdf (gpd.GeoDataFrame): GeoDataFrame with point or polygon
+            objects around which buffers are created. Must have the same coordinate system as the districts_gdf.
+        radius (float): The radius of the buffer zone in meters.
+    Return:
+        result_gdf (gpd.GeoDataFrame): A copy of the districts_gdf with the added 'coverage_area' column.
+            Area in square meters (float).
+    """
+    check_not_empty(districts_gdf, "districts_gdf")
+    check_not_empty(objects_gdf, "objects_gdf")
+    check_same_crs(districts_gdf, objects_gdf)
+
+    districts = districts_gdf.to_crs(epsg=3857)
+    objects = objects_gdf.to_crs(epsg=3857)
+
+    buffers = create_buffers(objects, radius)
+
+    union_buffer = buffers.geometry.union_all()
+
+    intersections = districts.geometry.intersection(union_buffer)
+    covered_areas = intersections.area
+
+    result_gdf = districts_gdf.copy()
+    result_gdf["coverage_area"] = covered_areas
+
+    return result_gdf
